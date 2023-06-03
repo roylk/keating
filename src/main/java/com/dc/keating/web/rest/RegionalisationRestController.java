9d67de3691cd9b8757be3b63edd22828a8a77354
roylk <crkkamga@gmail.com>
2023-06-03 16:12:25 +0200

    new commit

--- src/main/java/com/dc/keating/web/rest/RegionalisationRestController.java
+++ src/main/java/com/dc/keating/web/rest/RegionalisationRestController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.dc.keating.web.rest.regionalisation;
+package com.dc.keating.web.rest;
 
 import com.dc.keating.config.Reponse;
 import com.dc.keating.entities.KtPays;
