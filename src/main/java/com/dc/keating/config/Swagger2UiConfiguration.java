dd7153d84d63830864e913131994ec9edbb84078
roylk <crkkamga@gmail.com>
2023-06-03 12:22:06 +0200

    swagger on

---
+++ src/main/java/com/dc/keating/config/Swagger2UiConfiguration.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dc.keating.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+
+/**
+ *
+ * @author user
+ */
+
+@Configuration
+@EnableSwagger2
+public class Swagger2UiConfiguration extends WebMvcConfigurationSupport {
+    
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+      .select()
+      .apis(RequestHandlerSelectors.basePackage("com.dc.keating.web.rest")) // Remplacez "com.example.api" par le package de vos API
+      .paths(PathSelectors.any())
+      .build();
+  }
+
+ 
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) 
+  {
+    //enabling swagger-ui part for visual documentation
+    registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+    registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+  }
+    
+    
+}
