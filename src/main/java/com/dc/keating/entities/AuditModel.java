b2c317da80da14ca23e0131f8c2339022c613c3c
roylk <crkkamga@gmail.com>
2023-06-13 08:09:18 +0200

    localdatetime adjustment

--- src/main/java/com/dc/keating/entities/AuditModel.java
+++ src/main/java/com/dc/keating/entities/AuditModel.java
@@ -5,12 +5,16 @@
 package com.dc.keating.entities;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -19,23 +23,27 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
  *
  * @author user
  */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class AuditModel implements Serializable {
     
     private static final long serialVersionUID = 1L;
 
-    @Temporal(TemporalType.TIMESTAMP)
+    //@Temporal(TemporalType.TIMESTAMP)
     @Column(name = "date_creation", nullable = false, updatable = false)
     @CreatedDate
-    private Date dateCreation;
+    private LocalDateTime dateCreation;
 
-    @Temporal(TemporalType.TIMESTAMP)
+    //@Temporal(TemporalType.TIMESTAMP)
     @Column(name = "date_modification", nullable = false)
     @LastModifiedDate
-    private Date dateModification;
+    private LocalDateTime dateModification;
 
-    public Date getDateCreation() {
+   /* public Date getDateCreation() {
         return dateCreation;
     }
 
@@ -49,6 +57,6 @@ public abstract class AuditModel implements Serializable {
 
     public void setDateModification(Date dateModification) {
         this.dateModification = dateModification;
-    }
+    }*/
         
 }
